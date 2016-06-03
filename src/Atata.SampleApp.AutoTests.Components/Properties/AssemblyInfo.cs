e4f6644db1de086e960fe3596cf768c719a2c9b6
YevgeniyShunevych <eshunevich@gmail.com>
2016-06-03 16:15:34 +0300

    Add initial project files

---
+++ src/Atata.SampleApp.AutoTests.Components/Properties/AssemblyInfo.cs
@@ -0,0 +1,5 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Atata.SampleApp.AutoTests.Components")]
+[assembly: Guid("2517eb55-60c7-47d0-90c8-cea2fd713bb9")]
---
+++ src/CommonAssemblyInfo.cs
@@ -0,0 +1,10 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyCompany("Yevgeniy Shunevych")]
+[assembly: AssemblyProduct("Atata")]
+[assembly: AssemblyCopyright("Copyright © Yevgeniy Shunevych 2016")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
