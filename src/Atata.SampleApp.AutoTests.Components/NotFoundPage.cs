91c1896f9f975492f3b2eb893211b6d137061fc8
Yevgeniy Shunevych <eshunevich@gmail.com>
2016-06-24 20:20:35 +0300

    Add Page_NotFound test

---
+++ src/Atata.SampleApp.AutoTests.Components/NotFoundPage.cs
@@ -0,0 +1,11 @@
+﻿using _ = Atata.SampleApp.AutoTests.NotFoundPage;
+
+namespace Atata.SampleApp.AutoTests
+{
+    [Name("Page Not Found")]
+    [VerifyTitle]
+    [VerifyH1(TermMatch.Contains)]
+    public class NotFoundPage : AppPage<_>
+    {
+    }
+}
--- src/Atata.SampleApp.AutoTests/PageTest.cs
+++ src/Atata.SampleApp.AutoTests/PageTest.cs
@@ -6,10 +6,16 @@ namespace Atata.SampleApp.AutoTests
     public class PageTest : AutoTest
     {
         [Test]
-        public void Settings()
+        public void Page_Settings()
         {
             Login().
                 Menu.Account.Settings();
+        }
+
+        [Test]
+        public void Page_NotFound()
+        {
+            Go.To<NotFoundPage>(url: "not-found-page");
         }
     }
 }
