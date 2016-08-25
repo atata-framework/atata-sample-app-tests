0b5540870dc0f4389c50b6ea502865754c3c5135
Yevgeniy Shunevych <eshunevich@gmail.com>
2016-08-25 22:36:01 +0300

    Update validation functionality

--- src/Atata.SampleApp.AutoTests.Components/Controls/ValidationMessageList`1.cs
+++ src/Atata.SampleApp.AutoTests.Components/Controls/ValidationMessageList`1.cs
@@ -6,6 +6,11 @@ namespace Atata.SampleApp.AutoTests
     public class ValidationMessageList<TOwner> : ControlList<ValidationMessage<TOwner>, TOwner>
         where TOwner : PageObject<TOwner>
     {
+        public ValidationMessage<TOwner> this[Func<TOwner, IControl<TOwner>> controlSelector]
+        {
+            get { return For(controlSelector); }
+        }
+
         public ValidationMessage<TOwner> For(Func<TOwner, IControl<TOwner>> controlSelector)
         {
             var validationMessageDefinition = UIComponentResolver.GetControlDefinition(typeof(ValidationMessage<TOwner>));
--- src/Atata.SampleApp.AutoTests/SignInTest.cs
+++ src/Atata.SampleApp.AutoTests/SignInTest.cs
@@ -16,12 +16,12 @@ namespace Atata.SampleApp.AutoTests
         {
             Go.To<SignInPage>().
                 SignIn.Click().
-                ValidationMessages.For(x => x.Email).Should.Equal(ValidationMessages.Required).
-                ValidationMessages.For(x => x.Password).Should.Equal(ValidationMessages.Required).
+                ValidationMessages[x => x.Email].Should.Equal(ValidationMessages.Required).
+                ValidationMessages[x => x.Password].Should.Equal(ValidationMessages.Required).
                 Email.Set(Config.Account.Email).
                 Password.Set(Config.Account.Password + "!").
                 SignIn.Click().
-                ValidationMessages.For(x => x.Password).Should.Equal("or Email is invalid");
+                ValidationMessages[x => x.Password].Should.Equal("or Email is invalid");
         }
     }
 }
