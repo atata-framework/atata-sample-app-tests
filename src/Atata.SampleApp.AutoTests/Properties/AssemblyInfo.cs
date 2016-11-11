3464c9b00fe879cf5f1ecc94e4142762ae38a53e
YevgeniyShunevych <eshunevich@gmail.com>
2016-11-11 12:41:39 +0200

    Extract NUnit settings to NUnitSettings.cs file

--- src/Atata.SampleApp.AutoTests/Properties/AssemblyInfo.cs
+++ src/Atata.SampleApp.AutoTests/Properties/AssemblyInfo.cs
@@ -1,11 +1,5 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
-using NUnit.Framework;
 
 [assembly: AssemblyTitle("Atata.SampleApp.AutoTests")]
 [assembly: Guid("ebadb914-9630-4f25-bd16-c0ca760f57bf")]
-
-[assembly: LevelOfParallelism(4)]
-[assembly: Parallelizable(ParallelScope.Fixtures)]
-[assembly: SetCulture("en-us")]
-[assembly: SetUICulture("en-us")]
