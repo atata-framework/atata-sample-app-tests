45abfd0cd07895199ccdc4df8d079cbd22f6bcad
YevgeniyShunevych <eshunevich@gmail.com>
2017-02-28 16:33:55 +0200

    Refactor AppPage`1

--- src/Atata.SampleApp.AutoTests.Components/AppPage`1.cs
+++ src/Atata.SampleApp.AutoTests.Components/AppPage`1.cs
@@ -2,31 +2,31 @@
 
 namespace Atata.SampleApp.AutoTests
 {
-    public class AppPage<_> : Page<_>
-        where _ : AppPage<_>
+    public abstract class AppPage<TOwner> : Page<TOwner>
+        where TOwner : AppPage<TOwner>
     {
         public MainMenu Menu { get; private set; }
 
-        public class MainMenu : BSNavbar<_>
+        public class MainMenu : BSNavbar<TOwner>
         {
             [FindByClass("navbar-header")]
-            public LinkDelegate<HomePage, _> Home { get; private set; }
+            public LinkDelegate<HomePage, TOwner> Home { get; private set; }
 
-            public LinkDelegate<PlansPage, _> Plans { get; private set; }
+            public LinkDelegate<PlansPage, TOwner> Plans { get; private set; }
 
-            public LinkDelegate<UsersPage, _> Users { get; private set; }
+            public LinkDelegate<UsersPage, TOwner> Users { get; private set; }
 
-            public LinkDelegate<SignInPage, _> SignIn { get; private set; }
+            public LinkDelegate<SignInPage, TOwner> SignIn { get; private set; }
 
-            public LinkDelegate<SignUpPage, _> SignUp { get; private set; }
+            public LinkDelegate<SignUpPage, TOwner> SignUp { get; private set; }
 
             public AccountDropdown Account { get; private set; }
 
-            public class AccountDropdown : BSDropdown<_>
+            public class AccountDropdown : BSDropdown<TOwner>
             {
-                public LinkDelegate<SignInPage, _> SignOut { get; private set; }
+                public LinkDelegate<SignInPage, TOwner> SignOut { get; private set; }
 
-                public LinkDelegate<SettingsPage, _> Settings { get; private set; }
+                public LinkDelegate<SettingsPage, TOwner> Settings { get; private set; }
             }
         }
     }
