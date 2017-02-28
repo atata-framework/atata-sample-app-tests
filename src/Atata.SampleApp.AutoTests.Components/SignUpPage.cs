214e9a00cd2b52cd6745e45861fd716e733ef139
Yevgeniy Shunevych <eshunevich@gmail.com>
2017-02-28 10:30:39 +0200

    Add SignUp test

--- src/Atata.SampleApp.AutoTests.Components/SignUpPage.cs
+++ src/Atata.SampleApp.AutoTests.Components/SignUpPage.cs
@@ -7,5 +7,25 @@ namespace Atata.SampleApp.AutoTests
     [Url("signup")]
     public class SignUpPage : AppPage<_>
     {
+        public TextInput<_> FirstName { get; private set; }
+
+        public TextInput<_> LastName { get; private set; }
+
+        [RandomizeStringSettings("{0}@mail.com")]
+        public TextInput<_> Email { get; private set; }
+
+        public PasswordInput<_> Password { get; private set; }
+
+        public Select<Office?, _> Office { get; private set; }
+
+        [FindByName]
+        public RadioButtonList<Gender?, _> Gender { get; private set; }
+
+        [FindByLabel(TermMatch.StartsWith, "I agree to")]
+        public CheckBox<_> Agreement { get; private set; }
+
+        public ButtonDelegate<UserDetailsPage, _> SignUp { get; private set; }
+
+        public ValidationMessageList<_> ValidationMessages { get; private set; }
     }
 }
---
+++ src/Atata.SampleApp.AutoTests/SignUpTest.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+
+namespace Atata.SampleApp.AutoTests
+{
+    public class SignUpTest : AutoTest
+    {
+        [Test]
+        public void SignUp()
+        {
+            string firstName, lastName, email;
+            Office office = Office.Rome;
+            Gender gender = Gender.Female;
+
+            Go.To<SignUpPage>().
+                FirstName.SetRandom(out firstName).
+                LastName.SetRandom(out lastName).
+                Email.SetRandom(out email).
+                Password.SetRandom().
+                Office.Set(office).
+                Gender.Set(gender).
+                Agreement.Check().
+                SignUp().
+                    Menu.Account.SignOut.Should().Exist().
+                    Header.Should.Equal($"{firstName} {lastName}").
+                    Email.Should.Equal(email).
+                    Office.Should.Equal(office).
+                    Gender.Should.Equal(gender).
+                    Birthday.Should.Not.Exist().
+                    Notes.Should.Not.Exist();
+        }
+    }
+}
