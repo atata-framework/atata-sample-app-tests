f0b3a761ebe0a1eb6e39080eea248b03105adf02
YevgeniyShunevych <eshunevich@gmail.com>
2017-03-14 18:19:15 +0200

    Increase copyright year to 2017 in CommonAssemblyInfo.cs

--- src/CommonAssemblyInfo.cs
+++ src/CommonAssemblyInfo.cs
@@ -3,7 +3,7 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyCompany("Yevgeniy Shunevych")]
 [assembly: AssemblyProduct("Atata Framework")]
-[assembly: AssemblyCopyright("Copyright © Yevgeniy Shunevych 2016")]
+[assembly: AssemblyCopyright("Copyright © Yevgeniy Shunevych 2017")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: AssemblyVersion("0.11.0")]
