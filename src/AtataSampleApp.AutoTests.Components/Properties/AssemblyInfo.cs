9acfb7fb51049266ca26c0a7b9d76643e0c23d86
Yevgeniy Shunevych <eshunevich@gmail.com>
2017-05-19 21:32:00 +0300

    Rename solution and pojects by changing the name from "Atata.SampleApp.*" to "AtataSampleApp.*"

--- src/AtataSampleApp.AutoTests.Components/Properties/AssemblyInfo.cs
+++ src/AtataSampleApp.AutoTests.Components/Properties/AssemblyInfo.cs
@@ -1,5 +1,5 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("Atata.SampleApp.AutoTests.Components")]
+[assembly: AssemblyTitle("AtataSampleApp.AutoTests.Components")]
 [assembly: Guid("2517eb55-60c7-47d0-90c8-cea2fd713bb9")]
--- src/AtataSampleApp.AutoTests/Properties/AssemblyInfo.cs
+++ src/AtataSampleApp.AutoTests/Properties/AssemblyInfo.cs
@@ -1,5 +1,5 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("Atata.SampleApp.AutoTests")]
+[assembly: AssemblyTitle("AtataSampleApp.AutoTests")]
 [assembly: Guid("ebadb914-9630-4f25-bd16-c0ca760f57bf")]
