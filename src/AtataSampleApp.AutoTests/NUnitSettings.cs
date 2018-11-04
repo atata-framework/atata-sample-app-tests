aee81142461e9831b648700347bae0fc90691ed7
YevgeniyShunevych <yevgeniy.shunevych@gmail.com>
2018-11-04 19:10:10 +0200

    Move NUnitSettings.cs file from Properties folder to AtataSampleApp.AutoTests project root folder

---
+++ src/AtataSampleApp.AutoTests/NUnitSettings.cs
@@ -0,0 +1,6 @@
+﻿using NUnit.Framework;
+
+[assembly: LevelOfParallelism(4)]
+[assembly: Parallelizable(ParallelScope.Fixtures)]
+[assembly: SetCulture("en-US")]
+[assembly: SetUICulture("en-US")]
--- src/AtataSampleApp.AutoTests/Properties/NUnitSettings.cs
+++
@@ -1,6 +0,0 @@
-﻿using NUnit.Framework;
-
-[assembly: LevelOfParallelism(4)]
-[assembly: Parallelizable(ParallelScope.Fixtures)]
-[assembly: SetCulture("en-us")]
-[assembly: SetUICulture("en-us")]
