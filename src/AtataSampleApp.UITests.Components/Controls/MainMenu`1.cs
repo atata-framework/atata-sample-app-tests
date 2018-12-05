fa6caa439aced9d367dee6822928b1cfacf67068
YevgeniyShunevych <yevgeniy.shunevych@gmail.com>
2018-12-05 15:24:05 +0200

    Extract MainMenu from AppPage`1

---
+++ src/AtataSampleApp.UITests.Components/Controls/MainMenu`1.cs
@@ -0,0 +1,29 @@
+﻿using Atata;
+using Atata.Bootstrap;
+
+namespace AtataSampleApp.UITests
+{
+    public class MainMenu<TOwner> : BSNavbar<TOwner>
+        where TOwner : PageObject<TOwner>
+    {
+        [FindByClass("navbar-header")]
+        public LinkDelegate<HomePage, TOwner> Home { get; private set; }
+
+        public LinkDelegate<PlansPage, TOwner> Plans { get; private set; }
+
+        public LinkDelegate<UsersPage, TOwner> Users { get; private set; }
+
+        public LinkDelegate<SignInPage, TOwner> SignIn { get; private set; }
+
+        public LinkDelegate<SignUpPage, TOwner> SignUp { get; private set; }
+
+        public AccountDropdown Account { get; private set; }
+
+        public class AccountDropdown : BSDropdown<TOwner>
+        {
+            public LinkDelegate<SignInPage, TOwner> SignOut { get; private set; }
+
+            public LinkDelegate<SettingsPage, TOwner> Settings { get; private set; }
+        }
+    }
+}
