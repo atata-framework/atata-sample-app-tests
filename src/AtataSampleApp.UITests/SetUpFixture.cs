f1c8a8c5178a77a31eca136c5eb1af2e70c06167
YevgeniyShunevych <yevgeniy.shunevych@gmail.com>
2021-02-18 17:56:47 +0200

    Add and use Atata.WebDriverSetup package; Remove Selenium.WebDriver.ChromeDriver package; Change TargetFrameworkVersion to v4.7.2

---
+++ src/AtataSampleApp.UITests/SetUpFixture.cs
@@ -0,0 +1,33 @@
+﻿using Atata;
+using NUnit.Framework;
+
+namespace AtataSampleApp.UITests
+{
+    [SetUpFixture]
+    public class SetUpFixture
+    {
+        [OneTimeSetUp]
+        public void GlobalSetUp()
+        {
+            AtataContext.GlobalConfiguration
+                .ApplyJsonConfig<AtataConfig>()
+                .UseChrome()
+                    .WithArguments("start-maximized")
+                // Base URL can be set here, but in this sample it is defined in Atata.json config file.
+                //.UseBaseUrl("https://demo.atata.io/")
+                .UseCulture("en-US")
+                .UseAllNUnitFeatures()
+                // Configure logging:
+                //    .WithMinLevel(LogLevel.Info)
+                //    .WithoutSectionFinish().
+                .AddNLogLogging() // Actual NLog configuration is located in NLog.config file.
+                                  // Logs can be found in "{repo folder}\src\AtataSampleApp.UITests\bin\Debug\Logs\"
+                .AddScreenshotFileSaving()
+                // Below are the possible ways to specify the folder path to store screenshots for individual tests.
+                // Both examples build the same path which is used by default.
+                //    .WithFolderPath(@"Logs\{build-start}\{test-name}")
+                //    .WithFolderPath(() => $@"Logs\{AtataContext.BuildStart:yyyy-MM-dd HH_mm_ss}\{AtataContext.Current.TestName}")
+                .AutoSetUpDriverToUse();
+        }
+    }
+}
--- src/AtataSampleApp.UITests/UITestFixture.cs
+++ src/AtataSampleApp.UITests/UITestFixture.cs
@@ -14,30 +14,7 @@ namespace AtataSampleApp.UITests
         [SetUp]
         public void SetUp()
         {
-            AtataContext.Configure().
-                ApplyJsonConfig<AtataConfig>().
-                UseChrome().
-                    WithArguments("start-maximized", "disable-infobars", "disable-extensions").
-                // Base URL can be set here, but in this sample it is defined in Atata.json config file.
-                //UseBaseUrl("https://demo.atata.io/").
-                UseCulture("en-US").
-                UseNUnitTestName().
-                AddNUnitTestContextLogging().
-                // Configure logging:
-                //    WithMinLevel(LogLevel.Info).
-                //    WithoutSectionFinish().
-                AddNLogLogging().// Actual NLog configuration is located in NLog.config file.
-                                 // Logs can be found in "{repo folder}\src\AtataSampleApp.UITests\bin\Debug\Logs\"
-                AddScreenshotFileSaving().
-                // Below are possible ways to specify folder path to store screenshots for individual tests.
-                // Both examples build the same path which is used by default.
-                //    WithFolderPath(@"Logs\{build-start}\{test-name}").
-                //    WithFolderPath(() => $@"Logs\{AtataContext.BuildStart:yyyy-MM-dd HH_mm_ss}\{AtataContext.Current.TestName}").
-                LogNUnitError().
-                TakeScreenshotOnNUnitError().
-                UseAssertionExceptionType<NUnit.Framework.AssertionException>().
-                UseNUnitAggregateAssertionStrategy().
-                Build();
+            AtataContext.Configure().Build();
         }
 
         [TearDown]
