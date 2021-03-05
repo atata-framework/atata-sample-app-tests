2e6553e03cf0d74cd399ee0352aa078be72b6d3b
YevgeniyShunevych <yevgeniy.shunevych@gmail.com>
2021-03-05 17:56:53 +0200

    Change AtataSampleApp.UITests.Components project type to .NET Standard 2.0

--- src/AtataSampleApp.UITests.Components/Properties/AssemblyInfo.cs
+++
@@ -1,5 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("AtataSampleApp.UITests.Components")]
-[assembly: Guid("2517eb55-60c7-47d0-90c8-cea2fd713bb9")]
