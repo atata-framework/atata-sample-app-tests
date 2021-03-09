f6f7f3ed70cbff8f131e230bee7a69e7d6799944
YevgeniyShunevych <yevgeniy.shunevych@gmail.com>
2021-03-09 13:22:26 +0200

    Extract all Atata configuration to Atata.json

--- test/AtataSampleApp.UITests.Components/AtataSettings.cs
+++
@@ -1,6 +0,0 @@
-﻿using Atata;
-
-[assembly: VerifyTitleSettings(Format = "{0} - Atata Sample App")]
-
-// You can, for example, configure trigger to take screenshots before any click.
-//[assembly: Screenshot(On = TriggerEvents.BeforeClick, AppliesTo = TriggerScope.Children)]
